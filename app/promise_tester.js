d5139a106011e953c268765c8131a105f7e28cb3
Sai To Yeung <saitoyeung@gmail.com>
2017-03-01 18:10:02 -0500

    Use promise helper, and setTimeout may be faster than _.defer

--- app/promise_tester.js
+++ app/promise_tester.js
@@ -9,9 +9,10 @@ class PromiseTester {
     return axios.get('http://localhost:4567/id')
       .then((response) => {
         this.status = 'pending';
-        const id = response.id;
-        return axios.get('http://localhost:4567/items/' + id); })
-      .then((response) => { 
+        const id = response.data.id;
+        return axios.get('http://localhost:4567/items/' + id);
+      })
+      .then((response) => {
         this.status = 'finished';
 
         return response;
--- test/promise_tester_test.js
+++ test/promise_tester_test.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import PromiseTester from '../app/promise_tester.js';
 
 describe('Testing Promises', () => {
-  it('chaining promises using Q', (done) => {
+  it('can test chaining promises using Q', (done) => {
     const idDeferred = Q.defer();
     const itemDeferred = Q.defer();
 
@@ -16,15 +16,42 @@ describe('Testing Promises', () => {
 
     expect(promiseTester.status).toEqual('unstarted');
 
-    idDeferred.resolve({id: '1'});
+    idDeferred.resolve({data: {id: '1'}});
 
-    _.defer(() => {
+    setTimeout(() => {
       expect(promiseTester.status).toEqual('pending');
-      itemDeferred.resolve({id: '1', content: 'boom'});
-      _.defer(() => {
+      itemDeferred.resolve({data: {id: '1', content: 'boom'}});
+      setTimeout(() => {
         expect(promiseTester.status).toEqual('finished');
         done();
-      });
-    });
+      }, 0);
+    }, 0);
+  });
+
+  it('can test chaining promises using a promise Helper', (done) => {
+    const idPromiseHelper = {};
+    const itemPromiseHelper = {};
+
+    const idDeferred = new Promise((resolve, reject) => { idPromiseHelper.resolve = resolve; });
+    const itemDeferred = new Promise((resolve, reject) => { itemPromiseHelper.resolve = resolve; });
+
+    spyOn(axios, 'get').and.returnValues(idDeferred, itemDeferred);
+
+    const promiseTester = new PromiseTester();
+
+    promiseTester.fetchItem();
+
+    expect(promiseTester.status).toEqual('unstarted');
+
+    idPromiseHelper.resolve({data: {id: '1'}});
+
+    setTimeout(() => {
+      expect(promiseTester.status).toEqual('pending');
+      itemPromiseHelper.resolve({data: {id: '1', content: 'boom'}});
+      setTimeout(() => {
+        expect(promiseTester.status).toEqual('finished');
+        done();
+      }, 0);
+    }, 0);
   });
 });
