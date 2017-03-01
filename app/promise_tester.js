6981e2edd5726fd9a1c2c26e4e5ff5dec14e38bc
Sai To Yeung <saitoyeung@gmail.com>
2017-03-01 15:16:42 -0500

    Test for chained promises

--- app/promise_tester.js
+++ app/promise_tester.js
@@ -1,19 +1,21 @@
 import axios from 'axios';
 
 class PromiseTester {
-  constructor(global) {
-    this.value = 0;
-    this.global = global;
+  constructor() {
+    this.status = 'unstarted';
   }
 
   fetchItem() {
-    axios.get('http://localhost:4567/id').then((response) => {
-      const id = response.data.id;
-      return axios.get('http://localhost:4567/items/' + id); 
-    }).then((response) => {
-      const itemNode = this.global.document.getElementById('item-content');
-      itemNode.innerHTML = response.data.content;
-    });
+    return axios.get('http://localhost:4567/id')
+      .then((response) => {
+        this.status = 'pending';
+        const id = response.id;
+        return axios.get('http://localhost:4567/items/' + id); })
+      .then((response) => { 
+        this.status = 'finished';
+
+        return response;
+      });
   }
 }
 
--- test/promise_tester_test.js
+++ test/promise_tester_test.js
@@ -1,15 +1,30 @@
 import _ from 'lodash';
+import Q from 'q';
+import axios from 'axios';
 import PromiseTester from '../app/promise_tester.js';
 
 describe('Testing Promises', () => {
-  it('chaining promises', (done) => {
+  it('chaining promises using Q', (done) => {
+    const idDeferred = Q.defer();
+    const itemDeferred = Q.defer();
+
+    spyOn(axios, 'get').and.returnValues(idDeferred.promise, itemDeferred.promise);
+
     const promiseTester = new PromiseTester();
 
-    promiseTester.chainTimeouts();
+    promiseTester.fetchItem();
+
+    expect(promiseTester.status).toEqual('unstarted');
+
+    idDeferred.resolve({id: '1'});
 
     _.defer(() => {
-      expect(promiseTester.value).toEqual(1);
-      done();
+      expect(promiseTester.status).toEqual('pending');
+      itemDeferred.resolve({id: '1', content: 'boom'});
+      _.defer(() => {
+        expect(promiseTester.status).toEqual('finished');
+        done();
+      });
     });
   });
 });
